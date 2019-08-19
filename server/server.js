b3296a367ff6ec6ae630abc1afa2460c53faead5
DSTHEDRAGON <drakendev@yahoo.com>
2019-08-19 16:55:16 +0100

    Added Build

--- server/server.js
+++ server/server.js
@@ -157,6 +157,13 @@ app.delete("/api/delete_book", (req,res) =>{
     })
 })
 
+if(processe.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*', (req,res) => {
+        res.sendfile(path.resolve(__dirname),'../client','build','index/html')
+    })
+}
+
 const port = process.env.PORT || 3001
 app.listen(port,() =>{
     console.log("SERVER RUNNING")
